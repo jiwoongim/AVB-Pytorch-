f8e3d3990edb224b5aa64bcf68942dd611e6a5f9
Daniel <jiwoong.im90@gmail.com>
2018-03-30 12:56:05 -0400

    first push,, not woring atm

---
+++ AVB.py
@@ -0,0 +1,347 @@
+import utils, torch, time, os, pickle
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+#from torch.distributions.distribution import Distribution
+
+from utils import log_likelihood_samplesImean_sigma, prior_z, log_mean_exp
+
+torch.manual_seed(1)
+torch.cuda.manual_seed_all(1)
+
+PI2 = torch.log(torch.FloatTensor(np.asarray([np.pi]))*2)
+
+class Discriminator(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : (64)4c2s-(128)4c2s_BL-FC1024_BL-FC1_S
+    def __init__(self, z_dim, batch_size, arch_type, gpu_mode):
+        super(Discriminator, self).__init__()
+        self.input_height = 28
+        self.input_width = 28
+        self.input_dim = 1
+        self.z_dim = z_dim
+        self.output_dim=1
+        self.arch_type=arch_type
+        self.gpu_mode = gpu_mode
+        self.batch_size = batch_size
+
+        self.zlayer = nn.Sequential(
+            nn.Linear(self.z_dim, self.z_dim),
+            nn.BatchNorm1d(self.z_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.z_dim, 1),
+        )
+
+        self.zflayer = nn.Sequential(
+            nn.Linear(self.z_dim, self.z_dim),
+            nn.BatchNorm1d(self.z_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.z_dim, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.z_dim*4, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.LeakyReLU(0.2),
+
+        )
+
+        self.xlayer = nn.Sequential(
+            nn.Linear(self.input_height*self.input_width, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.ReLU(),
+            nn.Linear(self.z_dim*4, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.ReLU(),
+            nn.Linear(self.z_dim*4, 1),
+        )
+
+        self.xflayer = nn.Sequential(
+            nn.Linear(self.input_height*self.input_width, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.z_dim*4, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.LeakyReLU(0.2),
+            nn.Linear(self.z_dim*4, self.z_dim*4),
+            nn.BatchNorm1d(self.z_dim*4),
+            nn.LeakyReLU(0.2),
+
+        ) 
+        utils.initialize_weights(self)
+
+        if self.gpu_mode:
+            self.BCE_loss = nn.BCELoss().cuda()
+        else:
+            self.BCE_loss = nn.BCELoss()
+
+
+    def forward(self, X, Z):
+
+        H = self.logQZX_PZX(X, Z)
+        return F.sigmoid(H)
+
+
+    def logQZX_PZX(self, X, Z):
+
+        X = X.view([-1, self.input_height * self.input_width * self.input_dim])
+        Tx = self.xlayer(X)
+        Tz = self.zlayer(Z)
+
+        Hx = self.xflayer(X)
+        Hz = self.zflayer(Z)
+        H = torch.sum(Hx.mul_(Hz), dim=1) + torch.squeeze(Tx.add_(Tz))
+    
+        return H - 0.5 * torch.sum(Z**2, dim=1)
+
+
+    def loss(self, x_, z_x_, z_, ftype='gan'):
+
+        Y0 = self.forward(x_, z_x_)
+        Y1 = self.forward(x_, z_)
+        #Y0 = Y0.clamp(0.0001, 0.9999)
+        #Y1 = Y1.clamp(0.0001, 0.9999)
+        #J  = -torch.mean(torch.log(Y0) + torch.log(1 - Y1))
+        y_real_ = Variable(torch.ones(self.batch_size, 1).cuda())
+        y_fake_ = Variable(torch.zeros(self.batch_size, 1).cuda())
+        D_real_loss = self.BCE_loss(Y0, y_real_)
+        D_fake_loss = self.BCE_loss(Y1, y_fake_)
+        return D_real_loss+D_fake_loss
+
+
+
+class Encoder(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    def __init__(self, z_dim, arch_type, acF=False, gpu_mode=True, dim_sam=10):
+        super(Encoder, self).__init__()
+
+        # Architecture : (64)4c2s-(128)4c2s_BL-FC1024_BL-FC1_S
+        self.input_height = 28
+        self.input_width = 28
+        self.input_dim = 1
+        self.acF = acF
+        self.z_dim = z_dim
+        self.dim_sam = dim_sam
+        self.gpu_mode = gpu_mode
+        self.arch_type = arch_type
+
+        if self.arch_type == 'conv':
+            self.enc_layer1 = nn.Sequential(
+                nn.Conv2d(self.input_dim, 64, 4, 2, 1),
+                nn.LeakyReLU(0.2),
+                nn.Conv2d(64, 128, 4, 2, 1),
+                nn.BatchNorm2d(128),
+                nn.LeakyReLU(0.2),
+            )
+            self.fc = nn.Sequential(
+                nn.Linear(128 * (self.input_height // 4) * (self.input_width // 4), self.z_dim),
+            )
+        
+        else:
+
+            self.enc_layer1 = nn.Sequential(
+                nn.Linear(self.input_height*self.input_width\
+                                *self.input_dim, self.z_dim*4),
+                nn.BatchNorm1d(self.z_dim*4),
+                nn.LeakyReLU(0.2),
+                nn.Linear(self.z_dim*4, self.z_dim*4),
+                nn.BatchNorm1d(self.z_dim*4),
+                nn.LeakyReLU(0.2),
+                nn.Linear(self.z_dim*4, self.z_dim*4),
+                nn.BatchNorm1d(self.z_dim*4),
+                nn.LeakyReLU(0.2),
+            )
+
+            self.fc = nn.Sequential(
+                nn.Linear(self.z_dim*4, self.z_dim),
+            )
+
+            self.enc_nlayer1 = nn.Sequential(
+                nn.Linear(self.z_dim*4, self.z_dim*4),
+            )
+
+        if self.acF: 
+            self.mu = nn.Sequential(
+                nn.Linear(self.z_dim*4, self.z_dim),
+            )
+    
+            self.sigma = nn.Sequential(
+                nn.Linear(self.z_dim*4, self.z_dim),
+            )
+    
+        utils.initialize_weights(self)
+
+
+    def forward(self, x):
+
+        if self.arch_type == 'conv':
+            x = self.enc_layer1(x)
+            x = x.view(-1, 128 * (self.input_height // 4) * (self.input_width // 4))
+        else:
+
+            x = x.view([-1, self.input_height * self.input_width * self.input_dim])
+            x = self.enc_layer1(x)
+
+        if self.gpu_mode :
+            eps = torch.cuda.FloatTensor(x.size()).normal_()
+        else:
+            eps = torch.FloatTensor(x.size()).normal_()
+        eps = Variable(eps, requires_grad=False) 
+        h = self.fc(F.tanh(x + self.enc_nlayer1(eps)))
+
+        return h
+
+
+    def sample(self, mu, logvar):
+
+        std = torch.exp(logvar)
+        if self.gpu_mode :
+            eps = torch.cuda.FloatTensor(std.size()).normal_()
+        else:
+            eps = torch.FloatTensor(std.size()).normal_()
+        eps = Variable(eps)
+
+        return eps.mul(std).add_(mu)
+
+
+
+class Decoder(nn.Module):
+    # Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
+    # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+    def __init__(self, z_dim, arch_type):
+        super(Decoder, self).__init__()
+
+        # Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
+        self.input_height = 28
+        self.input_width = 28
+        self.output_dim = 1
+        self.z_dim = z_dim
+        self.arch_type = arch_type
+
+        if self.arch_type == 'conv':
+            self.dec_layer1 = nn.Sequential(
+                nn.Linear(self.z_dim, 128 * (self.input_height // 4) * (self.input_width // 4)),
+                nn.BatchNorm1d(128 * (self.input_height // 4) * (self.input_width // 4)),
+                nn.LeakyReLU(0.2),
+            )
+            self.dec_layer2 = nn.Sequential(
+                nn.ConvTranspose2d(128, 64, 4, 2, 1),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.ConvTranspose2d(64, self.output_dim, 4, 2, 1),
+                nn.Sigmoid(),
+            )
+        else:
+
+            self.dec_layer1 = nn.Sequential(
+                nn.Linear(self.z_dim, self.z_dim*4),
+                nn.BatchNorm1d(self.z_dim*4),
+                nn.LeakyReLU(0.2),
+                nn.Linear(self.z_dim*4, self.z_dim*4),
+                nn.BatchNorm1d(self.z_dim*4),
+                nn.Tanh(),
+            )
+
+            self.dec_layer2 = nn.Sequential(
+                nn.Linear(self.z_dim*4, self.input_height * self.input_width),
+                nn.Sigmoid(),
+            )
+
+
+        utils.initialize_weights(self)
+
+
+    def forward(self, z):
+
+        N,D = z.size()
+        x = self.dec_layer1(z.view([-1,D]))
+        if self.arch_type == 'conv':
+            x = x.view(-1, 128, (self.input_height // 4), (self.input_width // 4))
+            x = self.dec_layer2(x)
+        else:
+            x = self.dec_layer2(x)
+            x = x.view(-1, 1, self.input_height, self.input_width)
+
+        return x
+
+
+
+class AVB():
+
+    def __init__(self, args):
+        super(AVB, self).__init__()
+        # parameters
+        self.epoch = args.epoch
+        self.batch_size = args.batch_size
+        self.save_dir = args.save_dir
+        self.result_dir = args.result_dir
+        self.dataset = args.dataset
+        self.log_dir = args.log_dir
+        self.gpu_mode = args.gpu_mode
+        self.model_name = args.model_type
+        self.z_dim = args.z_dim
+        self.dim_sam=args.dim_sam
+        self.arch_type=args.arch_type
+        self.acF=args.acF
+
+        # Networks init
+        self.disc_net = Discriminator(self.z_dim, self.batch_size, self.arch_type, self.gpu_mode)
+        self.encoder  = Encoder(self.z_dim, self.arch_type, self.acF, self.gpu_mode, dim_sam=self.dim_sam)
+        self.decoder  = Decoder(self.z_dim, self.arch_type)
+
+        if self.gpu_mode:
+            self.disc_net = self.disc_net.cuda()
+            self.encoder  = self.encoder.cuda()
+            self.decoder  = self.decoder.cuda()
+
+        # fixed noise
+        if self.gpu_mode:
+            self.sample_z_ = Variable(torch.randn((self.batch_size, self.z_dim)).cuda(), volatile=True)
+        else:
+            self.sample_z_ = Variable(torch.randn((self.batch_size, self.z_dim)), volatile=True)
+
+    
+    def forward(self, x):
+
+        if self.model_name == 'DAVB':
+            if self.gpu_mode:
+                eps = torch.cuda.FloatTensor(x.size()).normal_(std=0.05)
+            else:
+                eps = torch.FloatTensor(x.size()).normal_(std=0.05)
+            x = x.add_(Variable(eps))
+        
+        z   = self.encoder(x)
+        res = self.decoder(z)
+        return res, z
+
+
+    def loss(self, x, beta):
+
+        N, C, iw, ih = x.size()
+        if self.acF:
+            z, mu, logvar = encoder(x_real)
+            mu, logvar = tf.stop_gradient(mean), tf.stop_gradient(logvar)
+            logstd = tf.sqrt(logvar + 1e-4)
+        else:
+            z   = self.encoder(x)
+        recon_x = self.decoder(z)
+
+
+        bce = x * torch.log(recon_x) + (1. - x) * torch.log(1 - recon_x)
+        bce = torch.sum(torch.sum(torch.sum(bce, dim=3), dim=2), dim=1)
+        
+        Td = self.disc_net.logQZX_PZX(x, z)
+        #logz = prior_z(z, dim=-1)
+        #logr = - 0.5 * torch.sum(z**2 + PI2[0], dim=-1) 
+        #logr = - 0.5 * torch.sum(z**2 + logstd*2 + PI2[0], dim=-1) 
+        KL = torch.squeeze(Td) #+ logr - logz
+        J  = torch.mean(bce - beta * KL, dim=0)
+        return -J
+
+
+
---
+++ main_avb.py
@@ -0,0 +1,278 @@
+import argparse, os, time, pickle
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import utils 
+from AVB import AVB
+
+def train(model, args, data_loader):
+
+
+    print('---------- Networks architecture -------------')
+    utils.print_network(model.disc_net)
+    utils.print_network(model.encoder)
+    utils.print_network(model.decoder)
+    print('-----------------------------------------------')
+
+    disc_optimizer = optim.Adam(model.disc_net.parameters(), lr=args.lrDisc, betas=(args.beta1, args.beta2))
+    enc_optimizer = optim.Adam(model.encoder.parameters(), lr=args.lrEnc, betas=(args.beta1, args.beta2))
+    dec_optimizer = optim.Adam(model.decoder.parameters(), lr=args.lrDec, betas=(args.beta1, args.beta2))
+
+
+    train_hist = {}
+    train_hist['tr_loss'] = []
+    train_hist['per_epoch_time'] = []
+    train_hist['total_time'] = []
+
+    model.disc_net.train()
+    model.encoder.train()
+    model.decoder.train()
+    print('training start!!')
+    start_time = time.time()
+    for epoch in range(args.epoch):
+
+        epoch_start_time = time.time()
+        for iter, (x_, y_) in enumerate(data_loader):
+            if iter * args.batch_size < 50000:
+                if iter == data_loader.dataset.__len__() // args.batch_size:
+                    break
+                
+                N, C, IW, IH = x_.size()
+                z_ = torch.randn((args.batch_size, args.z_dim))
+                if args.gpu_mode:
+                    x_ = Variable(x_.cuda())
+                    z_ = Variable(z_.cuda())
+                else:
+                    x_ = Variable(x_)
+                    z_ = Variable(z_)
+
+                #if epoch < 150 or (epoch >= 150 and epoch %2 == 0):
+                #if np.random.randint(2, size=1)[0]:
+                for i in range(20):
+                    z_x_= model.encoder(x_)
+                    z_x_= torch.autograd.Variable(z_x_.data, requires_grad=False)
+
+                    # update Discriminator network 
+                    lrDisc = max([args.lrDisc / (1.0 + epoch / 100.0), 0.000001])
+                    disc_optimizer.zero_grad()
+                    lossD = model.disc_net.loss(x_, z_x_, z_)
+                    lossD.backward()
+                    #for g in disc_optimizer.param_groups: g['lr'] = lrDisc
+                    disc_optimizer.step() 
+
+
+                # update Encoder & Decoder network
+                enc_optimizer.zero_grad()
+                dec_optimizer.zero_grad()
+                lrEnc = max([args.lrEnc / (1.0 + epoch / 100.0), 0.000001])
+                lrDec = max([args.lrDec / (1.0 + epoch / 100.0), 0.000001])
+                for g in enc_optimizer.param_groups: g['lr'] = lrEnc
+                for g in dec_optimizer.param_groups: g['lr'] = lrDec
+                beta = min([float(epoch) / args.anneal_steps, 1.0])
+                loss = model.loss(x_, beta)
+                loss.backward()
+                train_hist['tr_loss'].append(loss.data[0])
+
+
+
+                if np.isnan(loss.data.cpu().numpy()):
+                    print('loss AVB nan')
+                    import pdb; pdb.set_trace()
+
+                # `clip_grad_norm` helps prevent the exploding gradient problem.
+                torch.nn.utils.clip_grad_norm(model.encoder.parameters(), args.clip)
+                enc_optimizer.step()
+                dec_optimizer.step()
+
+                if ((iter + 1) % 100) == 0:
+                    print("Epoch: [%2d] [%4d/%4d] Beta %.2f, lossIW: %.8f | lrD %.5f, lossD: %.8f" %
+                            ((epoch + 1), \
+                            (iter + 1), \
+                            len(data_loader.dataset) // args.batch_size, \
+                            beta, loss.data[0], lrDisc, lossD.data[0]))
+
+        train_hist['per_epoch_time'].append(time.time() - epoch_start_time)
+        visualize_results(model, epoch+1, args)
+
+        if epoch % 25 :
+            save(model, epoch, args.save_dir, args.dataset, \
+                    args.model_type, args.batch_size, train_hist)
+
+    train_hist['total_time'].append(time.time() - start_time)
+    print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % \
+                    (np.mean(train_hist['per_epoch_time']),
+                    epoch, train_hist['total_time'][0]))
+    print("Training finish!... save training results")
+
+    save(model, epoch, args.save_dir, args.dataset, args.model_type, \
+                                    args.batch_size, train_hist)
+    utils.generate_animation(args.result_dir + '/' + args.dataset + '/' \
+                    + args.model_type + '/' + args.model_type, epoch)
+    utils.loss_plot(train_hist, os.path.join(args.save_dir, args.dataset, \
+                                    args.model_type), args.model_type)
+
+
+def visualize_results(model, epoch, args, sample_num=100, fix=True):
+
+    model.disc_net.eval()
+    model.encoder.eval()
+    model.decoder.eval()
+
+    if not os.path.exists(args.result_dir + '/' + args.dataset + '/' + args.model_type):
+        os.makedirs(args.result_dir + '/' + args.dataset + '/' + args.model_type)
+
+    tot_num_samples = min(sample_num, args.batch_size)
+    image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
+
+    if fix:
+        """ fixed noise """
+        samples = model.decoder(model.sample_z_)
+    else:
+        """ random noise """
+        if args.gpu_mode:
+            sample_z_ = Variable(torch.rand((args.batch_size, 1, args.z_dim)).cuda(), volatile=True)
+        else:
+            sample_z_ = Variable(torch.rand((args.batch_size, 1, args.z_dim)), volatile=True)
+
+        samples = model.sample(sample_z_)
+
+    #N,C,IW,IH = samples.size()
+    #samples = samples.view([N,C,IW,IH])
+    if args.gpu_mode:
+        samples = samples.cpu().data.numpy().transpose(0, 2, 3, 1)
+    else:
+        samples = samples.data.numpy().transpose(0, 2, 3, 1)
+
+    utils.save_images(samples[:image_frame_dim * image_frame_dim, :, :, :], [image_frame_dim, image_frame_dim],
+                      args.result_dir + '/' + args.dataset + '/' + args.model_type + '/' + args.model_type + '_epoch%03d' % epoch + '.png')
+
+
+def save(model, epoch, save_dir, dataset, model_type, batch_size, train_hist):
+    save_dir = os.path.join(save_dir, dataset, model_type)
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    torch.save(model.encoder.state_dict(), os.path.join(save_dir, model_type + '_encoder_epoch' + str(epoch)+'_batch_sz' + str(batch_size)+'_encoder.pkl'))
+    torch.save(model.decoder.state_dict(), os.path.join(save_dir, model_type + '_encoder_epoch' + str(epoch)+'_batch_sz' + str(batch_size)+'_decoder.pkl'))
+    torch.save(model.disc_net.state_dict(), os.path.join(save_dir, model_type + '_encoder_epoch' + str(epoch)+'_batch_sz' + str(batch_size)+'disc_net.pkl'))
+
+    with open(os.path.join(save_dir, model_type + '_history.pkl'), 'wb') as f:
+        pickle.dump(train_hist, f)
+
+
+def load(model, save_dir, dataset='MNIST', model_type='AVB'):
+
+    save_dir = os.path.join(save_dir, dataset, model_type)
+    model.encoder.load_state_dict(torch.load(os.path.join(save_dir, model_type + '_encoder.pkl')))
+    model.decoder.load_state_dict(torch.load(os.path.join(save_dir, model_type + '_decoder.pkl')))
+    model.disc_net.load_state_dict(torch.load(os.path.join(save_dir, model_type + '_disc_net.pkl')))
+
+
+
+
+"""parsing and configuration"""
+def parse_args():
+    desc = "Pytorch implementation of AVB collections"
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument('--model_type', type=str, default='AVB',
+                        choices=['AVB', 'DAVB'],
+                        help='The type of AVB')#, required=True)
+    parser.add_argument('--arch_type', type=str, default='fc',
+                        choices=['conv', 'fc'],
+                        help='The Architecture Type')#, required=True)
+    parser.add_argument('--dataset', type=str, default='mnist', choices=['mnist'],
+                        help='The name of dataset')
+    parser.add_argument('--epoch', type=int, default=500, help='The number of epochs to run')
+    parser.add_argument('--batch_size', type=int, default=100, help='The size of batch')
+    parser.add_argument('--save_dir', type=str, default='models',
+                        help='Directory name to save the model')
+    parser.add_argument('--result_dir', type=str, default='results',
+                        help='Directory name to save the generated images')
+    parser.add_argument('--log_dir', type=str, default='logs',
+                        help='Directory name to save training logs')
+    parser.add_argument('--acF', type=bool, default=False)
+    parser.add_argument('--z_dim', type=float, default=128)
+    parser.add_argument('--dim_sam', type=float, default=10)
+    parser.add_argument('--lrDisc', type=float, default=1e-4)
+    parser.add_argument('--lrDec', type=float, default=1e-4)
+    parser.add_argument('--lrEnc', type=float, default=1e-4)
+    parser.add_argument('--anneal_steps', type=float, default=25)
+    parser.add_argument('--beta1', type=float, default=0.9)
+    parser.add_argument('--beta2', type=float, default=0.999)
+    parser.add_argument('--clip', type=float, default=5.0)
+    parser.add_argument('--gpu_mode', type=bool, default=True)
+
+    return check_args(parser.parse_args())
+
+
+"""checking arguments"""
+def check_args(args):
+    # --save_dir
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # --result_dir
+    if not os.path.exists(args.result_dir):
+        os.makedirs(args.result_dir)
+
+    # --result_dir
+    if not os.path.exists(args.log_dir):
+        os.makedirs(args.log_dir)
+
+    # --epoch
+    try:
+        assert args.epoch >= 1
+    except:
+        print('number of epochs must be larger than or equal to one')
+
+    # --batch_size
+    try:
+        assert args.batch_size >= 1
+    except:
+        print('batch size must be larger than or equal to one')
+
+    return args
+
+
+"""main"""
+def main():
+    # parse arguments
+    args = parse_args()
+    if args is None:
+        exit()
+
+
+    # declare instance for AVB
+    if args.model_type == 'AVB' or args.model_type == 'DAVB' :
+        model = AVB(args)
+    else:
+        raise Exception("[!] There is no option for " + args.model_type)
+
+
+    # load dataset
+    data_loader = DataLoader(datasets.MNIST('data/mnist', train=True, download=True,
+                                                        transform=transforms.Compose(
+                                                            [transforms.ToTensor()])),
+                                            batch_size=args.batch_size, shuffle=False)
+
+
+    # launch the graph in a session
+    train(model, args, data_loader)
+    print(" [*] Training finished!")
+
+    # visualize learned generator
+    model.visualize_results(args.epoch)
+    print(" [*] Testing finished!")
+
+
+
+
+if __name__ == '__main__':
+    main()
+
